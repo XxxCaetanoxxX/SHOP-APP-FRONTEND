ddd9ac6568d5321aea5d6580db7f45dd46c6c5af
caetano.cesar <caetano.cesar@defensoria.mg.def.br>
2024-12-03 16:14:21 -0300

    feat: implementando cubit para swtich das configurações

--- lib/src/core/bloc/index_dashboard.cubit.dart
+++ lib/src/core/bloc/index_dashboard.cubit.dart
@@ -4,7 +4,6 @@ class IndexDashboardCubit extends Cubit<int> {
   IndexDashboardCubit() : super(0);
 
   void selecionarIndex(int index) {
-    print("index atual: ${index}");
     emit(index);
   }
 
